8234b19d457b597e7e98e89121309d235e8e9d04
Nguyen Ha <nguyenxuanha168800@gmail.com>
2023-05-29 22:13:28 +0700

    Implement get Product by Category

--- BlazorEC_Ver2/Client/Services/ProductService/IProductService.cs
+++ BlazorEC_Ver2/Client/Services/ProductService/IProductService.cs
@@ -4,8 +4,9 @@ namespace BlazorEC_Ver2.Client.Services.ProductService
 {
     public interface IProductService
     {
+        event Action ProductsChange;
         List<Product> Products { get; set; }
-        Task GetProducts();
+        Task GetProducts(string? categoryUrl = null);
         Task<ServiceResponse<Product>> GetProduct(int productId);
     }
 }
--- BlazorEC_Ver2/Server/Controllers/ProductController.cs
+++ BlazorEC_Ver2/Server/Controllers/ProductController.cs
@@ -12,17 +12,26 @@ namespace BlazorEC_Ver2.Server.Controllers
         public ProductController(IProductService productService)
         {
             _productService = productService;
-        }    
+        }  
+        
         [HttpGet]
         public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProduct()
         {
             var result = await _productService.GetProductsAsync();
             return Ok(result);
         }
+
         [HttpGet("{productId}")]
         public async Task<ActionResult<ServiceResponse<Product>>> GetProduct(int productId)
         {
             var result = await _productService.GetProductAsync(productId);
+            return Ok(result);
+        }
+
+        [HttpGet("category/{categoryUrl}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductByCategory(string categoryUrl)
+        {
+            var result = await _productService.GetProductByCategory(categoryUrl);
             return Ok(result);
         }
     }
