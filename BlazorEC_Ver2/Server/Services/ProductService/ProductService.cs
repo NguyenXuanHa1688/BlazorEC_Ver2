198114c513d33e182cd1bd9137dd603046368dec
Nguyen Ha <nguyenxuanha168800@gmail.com>
2023-05-31 16:43:24 +0700

    Implement Product variants into client side

--- BlazorEC_Ver2/Server/Services/ProductService/ProductService.cs
+++ BlazorEC_Ver2/Server/Services/ProductService/ProductService.cs
@@ -10,8 +10,11 @@
 
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
-            var response = new ServiceResponse<Product>(); 
-            var product = await _context.Products.FindAsync(productId);
+            var response = new ServiceResponse<Product>();
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
             if(product == null)
             {
                 response.Success = false;
@@ -28,7 +31,9 @@
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await _context.Products.Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower())).ToListAsync()
+                Data = await _context.Products.Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                .Include(p => p.Variants)
+                .ToListAsync()
             };
             return response;
         }
@@ -37,7 +42,9 @@
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products
+                    .Include(p => p.Variants)
+                    .ToListAsync()
             };
             return response;
         }
